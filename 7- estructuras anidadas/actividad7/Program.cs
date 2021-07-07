25b62c68fd2675a9b8cfaae77d822b0c99c9398b
Fernando Herrera <69762889+1w3112006UtreraAgustin@users.noreply.github.com>
2021-07-07 00:42:07 -0300

    commit 2 actividades s Sharp ya
    Entrega de actividades 7 y 8 del tutorial.

---
+++ 7- estructuras anidadas/actividad7/Program.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace actividad7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            //string aux;
+            //int nota1, nota2, nota3;
+            //double promedio;
+            //Console.WriteLine("Ingresa la primer nota del alumno: ");
+            //aux = Console.ReadLine();
+            //nota1 = Convert.ToInt32(aux);
+            //Console.WriteLine("Ingresa la segunda nota del alumno: ");
+            //aux = Console.ReadLine();
+            //nota2 = Convert.ToInt32(aux);
+            //Console.WriteLine("Ingresa la tercer nota del alumno: ");
+            //aux = Console.ReadLine();
+            //nota3 = Convert.ToInt32(aux);
+            //promedio = (nota1 + nota2 + nota3) / 3;
+            //if(promedio>=7)
+            //{
+            //    Console.Write("Promocionado");
+            //}
+            //else
+            //{
+            //    if(promedio>=4)
+            //    {
+            //        Console.Write("Regular");
+            //    }
+            //    else
+            //    {
+            //        Console.Write("Reprobado");
+            //    }
+            //}
+
+            //Console.ReadKey();
+
+
+
+            //1)Se cargan por teclado tres números distintos. Mostrar por pantalla el mayor de ellos.
+
+            //string aux;
+            //int num1, num2, num3;
+
+            //Console.WriteLine("Ingrese un número: ");
+            //aux = Console.ReadLine();
+            //num1 = Convert.ToInt32(aux);
+            //Console.WriteLine("Ingrese un segundo número: ");
+            //aux = Console.ReadLine();
+            //num2 = Convert.ToInt32(aux);
+            //Console.WriteLine("Ingrese un tercer número: ");
+            //aux = Console.ReadLine();
+            //num3 = Convert.ToInt32(aux);
+
+            //if (num1>num2 && num1>num3)
+            //{
+            //    Console.Write("El primer número ingresado es el mayor: "+num1);
+            //}
+            //else
+            //{
+            //    if (num2>num1 && num2>num3)
+            //    {
+            //        Console.Write("El segundo número ingresado es el mayor: "+num2);
+            //    }
+            //    else
+            //    {
+            //        Console.Write("El tercer número ingresado es el mayor: "+num3);
+            //    }
+            //}
+
+            //Console.ReadKey();
+
+
+
+            //2)Se ingresa por teclado un valor entero,
+            //mostrar una leyenda que indique si el número es positivo, nulo o negativo. 
+            //string aux;
+            //int entero;
+            //Console.WriteLine("Ingrese un valor entero: ");
+            //aux = Console.ReadLine();
+            //entero = int.Parse(aux);
+            //if(entero>0)
+            //{
+            //    Console.Write("El numero ingresado es positivo.");
+            //}
+            //else
+            //{
+            //    if(entero==0) 
+            //    {
+            //        Console.Write("El numero ingresado es nulo");
+            //    }
+            //    else
+            //    {
+            //        Console.Write("El numero ingresado es negativo");
+            //    }
+            //}
+            //Console.ReadKey();
+
+            //3)Confeccionar un programa que permita cargar un número entero positivo de hasta tres cifras y
+            //muestre un mensaje indicando si tiene 1, 2, o 3 cifras.
+            //Mostrar un mensaje de error si el número de cifras es mayor
+
+
+            //string aux;
+            //int num;
+            //Console.WriteLine("Ingresar un numero entero positivo de hasta tres cifras cualquiera: ");
+            //aux = Console.ReadLine();
+            //num = int.Parse(aux);
+            //if(num > -1 && num<10)
+            //{
+            //    Console.Write("El numero ingresado es de 1 cifra");
+            //}
+            //else
+            //{
+            //    if(num>9 && num<100 )
+            //    {
+            //        Console.Write("El numero ingresado es de 2 cifras");
+
+            //    }
+            //    else
+            //    {
+            //        if(num>99 && num<=999)
+            //        {
+            //            Console.Write("El numero ingresado es de 3 cifras"); 
+            //        }
+            //        else
+            //        {
+            //            Console.Write("Error, el numero no es positivo o es mayor a 3 cifras!");
+            //        }
+            //    }
+            //}
+            //Console.ReadKey();
+
+
+            //4)Un postulante a un empleo, realiza un test de capacitación,
+            //se obtuvo la siguiente información: cantidad total de preguntas que se le realizaron y
+            //la cantidad de preguntas que contestó correctamente.
+            //Se pide confeccionar un programa que ingrese los dos datos por teclado e
+            //informe el nivel del mismo según el porcentaje de respuestas correctas que ha obtenido,
+            //y sabiendo que:
+
+            //Nivel máximo:	Porcentaje >= 90 %.
+
+            //Nivel medio:	Porcentaje >= 75 % y < 90 %.
+
+            //Nivel regular:	Porcentaje >= 50 % y < 75 %.
+
+            //Fuera de nivel: Porcentaje < 50 %.
+
+            //string aux;
+            //int preguntas,respuestas;
+            //float porcentaje;
+            //Console.WriteLine("Ingrese la cantidad de preguntas que se le hicieron al postulante: ");
+            //aux = Console.ReadLine();
+            //preguntas = int.Parse(aux);
+            //Console.WriteLine("Ingrese la cantidad de respuestas acertadas por el postulante: ");
+
+            //aux = Console.ReadLine();
+            //respuestas = int.Parse(aux);
+
+            //porcentaje = (100 * respuestas) / preguntas;
+
+           
+
+            //if(porcentaje >= 90)
+            //{
+            //    Console.Write("Nivel máximo");
+            //}
+            //else
+            //{
+            //    if(porcentaje >= 75 && porcentaje < 90)
+            //    {
+            //        Console.Write("Nivel medio");
+            //    }
+            //    else
+            //    {
+            //        if(porcentaje >= 50 && porcentaje < 75  )
+            //        {
+            //            Console.Write("Nivel regular");
+            //        }
+            //        else
+            //        {
+            //            Console.Write("Fuera de nivel");
+            //        }
+            //    }
+            //}
+
+
+            //Console.ReadKey();
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ 8-Condiciones compuestas con operadores lógicos/condicionesCompuestas/condicionesCompuestas/Program.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace condicionesCompuestas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //string aux;
+            //int dia, mes, anio;
+            //Console.WriteLine("Ingrese el dia");
+            //aux = Console.ReadLine();
+            //dia = int.Parse(aux);
+            //Console.WriteLine("Ingrese el mes");
+            //aux = Console.ReadLine();
+            //mes = int.Parse(aux);
+            //Console.WriteLine("Ingrese el año");
+            //aux = Console.ReadLine();
+            //anio = int.Parse(aux);
+            //if(mes==1 || mes==2 || mes==3)
+            //{
+            //    Console.Write("La fecha ingresada corresponde al primer trimestre del año");
+            //}
+            //else
+            //{
+            //    if(mes>3 && mes<7)
+            //    {
+            //        Console.Write("La fecha ingresada corresponde al segundo trimestre del año");
+            //    }
+            //    else
+            //    {
+            //        Console.Write("no interesa");
+            //    }
+            //}
+            //Console.ReadKey();
+
+
+            //1)Realizar un programa que pida cargar una fecha cualquiera,
+            //luego verificar si dicha fecha corresponde a Navidad.
+            //
+            //string aux;
+            //int dia, mes, anio;
+            //Console.WriteLine("Ingrese el dia");
+            //aux = Console.ReadLine();
+            //dia = int.Parse(aux);
+            //Console.WriteLine("Ingrese el mes");
+            //aux = Console.ReadLine();
+            //mes = int.Parse(aux);
+            //Console.WriteLine("Ingrese el año");
+            //aux = Console.ReadLine();
+            //anio = int.Parse(aux);
+            //if (mes == 12 && dia==25)
+            //{
+            //    Console.Write("La fecha ingresada corresponde navidad");
+            //}
+            //Console.ReadKey();
+
+
+
+            //2)Se ingresan tres valores por teclado,
+            //si todos son iguales se imprime la suma del primero con el segundo y
+            //a este resultado se lo multiplica por el tercero. 
+
+
+            //string aux;
+            //int num1, num2, num3,suma,producto;
+            //Console.WriteLine("Ingrese el primer numero: ");
+            //aux = Console.ReadLine();
+            //num1 = int.Parse(aux);
+            //Console.WriteLine("Ingrese el segundo numero: ");
+            //aux = Console.ReadLine();
+            //num2 = int.Parse(aux);
+            //Console.WriteLine("Ingrese el tercer numero: ");
+            //aux = Console.ReadLine();
+            //num3 = int.Parse(aux);
+
+            //if(num1==num2 && num2==num3)
+            //{
+            //    suma = num1 + num2;
+            //    producto = suma * num3;
+            //    Console.Write("Suma de los dos primeros: " + suma + " producto de la suma por el tercer numero: " + producto);
+            //}
+            //else
+            //{
+            //    Console.Write("Error ingrese de nuevo los numeros");
+            //}
+            //Console.ReadKey();
+
+            //3)Se ingresan por teclado tres números, si todos los valores ingresados son menores a 10,
+            //imprimir en pantalla la leyenda "Todos los números son menores a diez". 
+
+
+            //string aux;
+            //int num1, num2, num3;
+            //Console.WriteLine("Ingrese el primer numero: ");
+            //aux = Console.ReadLine();
+            //num1 = int.Parse(aux);
+            //Console.WriteLine("Ingrese el segundo numero: ");
+            //aux = Console.ReadLine();
+            //num2 = int.Parse(aux);
+            //Console.WriteLine("Ingrese el tercer numero: ");
+            //aux = Console.ReadLine();
+            //num3 = int.Parse(aux);
+
+            //if (num1<10 && num2<10 && num3<10)
+            //{
+
+            //    Console.Write("Todos los numeros ingresados son menores a diez ");
+            //}
+
+            //Console.ReadKey();
+
+
+            //4)Se ingresan por teclado tres números, si al menos uno de los valores ingresados es menor a 10,
+            //imprimir en pantalla la leyenda "Alguno de los números es menor a diez". 
+
+
+            //string aux;
+            //int num1, num2, num3;
+            //Console.WriteLine("Ingrese el primer numero: ");
+            //aux = Console.ReadLine();
+            //num1 = int.Parse(aux);
+            //Console.WriteLine("Ingrese el segundo numero: ");
+            //aux = Console.ReadLine();
+            //num2 = int.Parse(aux);
+            //Console.WriteLine("Ingrese el tercer numero: ");
+            //aux = Console.ReadLine();
+            //num3 = int.Parse(aux);
+
+            //if (num1 < 10 || num2 < 10 || num3 < 10)
+            //{
+
+            //    Console.Write("Alguno de los numeros ingresados es menor a diez ");
+            //}
+
+            //Console.ReadKey();
+
+
+
+            //5)Escribir un programa que pida ingresar la coordenada de un punto en el plano,
+            //es decir dos valores enteros x e y (distintos a cero).
+            // Posteriormente imprimir en pantalla en que cuadrante se ubica dicho punto.
+            // (1º Cuadrante si x > 0 Y y > 0, 2º Cuadrante: x < 0 Y y > 0, etc.)
+
+
+
+            // string aux;
+            // int x, y;
+            // Console.WriteLine("Ingrese coordenada x: ");
+            // aux = Console.ReadLine();
+            // x = int.Parse(aux);
+            // Console.WriteLine("Ingrese coordenada y: ");
+            // aux = Console.ReadLine();
+            // y = int.Parse(aux);
+            //if(x>0 && y >0)
+            // {
+            //     Console.Write("las coordenadas ingresadas pertenecen al primer cuadrante");
+            // }
+            //else
+            // {
+            //     if(x<0 && y>0)
+            //     {
+            //         Console.Write("las coordenadas ingresadas pertenecen al segundo cuadrante");
+            //     }
+            //     else
+            //         if(x<0 && y<0)
+            //     {
+            //         Console.Write("las coordenadas ingresadas pertenecen al tercer cuadrante");
+            //     }
+            //     else
+            //     {
+            //         Console.Write("las coordenadas ingresadas pertenecen al cuarto cuadrante");
+            //     }
+            // }
+
+            // Console.ReadKey();
+
+
+            //6)De un operario se conoce su sueldo y los años de antigüedad.
+            //Se pide confeccionar un programa que lea los datos de entrada e informe:
+            //a) Si el sueldo es inferior a 500 y su antigüedad es igual o superior a 10 años,
+            //    otorgarle un aumento del 20 %, mostrar el sueldo a pagar.
+            //b)Si el sueldo es inferior a 500 pero su antigüedad es menor a 10 años, otorgarle un aumento de 5 %.
+            //c) Si el sueldo es mayor o igual a 500 mostrar el sueldo en pantalla sin cambios.
+
+
+            // string aux;
+            // double sueldo, antiguedad;
+            // Console.WriteLine("Ingrese el sueldo del operario: ");
+            // aux = Console.ReadLine();
+            //sueldo= int.Parse(aux);
+            // Console.WriteLine("Ingrese la antiguedad del operario: ");
+            // aux = Console.ReadLine();
+            // antiguedad = int.Parse(aux);
+
+            // if(sueldo<500 && antiguedad>=10)
+            // {
+            //     sueldo = sueldo * 1.2;
+            //     Console.Write("el sueldo a pagar es de : "+sueldo);
+            // }
+
+            // else
+            // {
+            //     if(sueldo<500 && antiguedad<10)
+            //     {
+            //         sueldo = sueldo * 1.05;
+            //         Console.Write("el sueldo a pagar es de : " + sueldo);
+            //     }
+            //     else
+            //     {
+            //         Console.Write("el sueldo a pagar es de : " + sueldo);
+            //     }
+            // }
+
+            // Console.ReadKey();
+
+
+
+
+            //7)Escribir un programa en el cual: dada una lista de tres valores numéricos distintos se
+            //calcule e informe su rango de variación
+            //(debe mostrar el mayor y el menor de ellos) 
+
+            //int num1, num2, num3;
+            //string linea;
+            //Console.Write("Ingrese primer valor:");
+            //linea = Console.ReadLine();
+            //num1 = int.Parse(linea);
+            //Console.Write("Ingrese segundo valor:");
+            //linea = Console.ReadLine();
+            //num2 = int.Parse(linea);
+            //Console.Write("Ingrese tercer valor:");
+            //linea = Console.ReadLine();
+            //num3 = int.Parse(linea);
+            //Console.Write("Rango de valores:");
+            //if (num1 < num2 && num1 < num3)
+            //{
+            //    Console.Write(num1);
+            //}
+            //else
+            //{
+            //    if (num2 < num3)
+            //    {
+            //        Console.Write(num2);
+            //    }
+            //    else
+            //    {
+            //        Console.Write(num3);
+            //    }
+            //}
+            //Console.Write("-");
+            //if (num1 > num2 && num1 > num3)
+            //{
+            //    Console.Write(num1);
+            //}
+            //else
+            //{
+            //    if (num2 > num3)
+            //    {
+            //        Console.Write(num2);
+            //    }
+            //    else
+            //    {
+            //        Console.Write(num3);
+            //    }
+            //}
+            //Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
